d01c7a81f87a93d1c9f9c0565968e0f842f913cf
himansudev <himansu.developer@gmail.com>
2020-06-06 03:39:57 +0530

    transformed

--- server.js
+++ server.js
@@ -1,31 +1,68 @@
 const express = require('express');
 const hbs = require('hbs');
-const port = process.env.PORT || 3000;
+const fs = require('fs');
+
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 
-app.set('view engine','hbs');
+// app.use((req, res, next) => {
+//   var now = new Date().toString();
+//   var log = `${now}: ${req.method} ${req.url}`;
 
-app.use(express.static(__dirname + '/public'));
+//   console.log(log);
+//   fs.appendFile('server.log', log + '\n');
+//   next();
+// });
 
-app.get('/',(req, res) => {
-    res.send('<h1>Hello Express!<h1>');
+// // app.use((req, res, next) => {
+// //   res.render('maintenance.hbs');
+// // });
+
+// app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
 });
 
-app.get('/home', (req, res) => {
-    res.render('home.hbs', {
-        welcomeMessage:'welcome'
-    })
+// hbs.registerHelper('screamIt', (text) => {
+//   return text.toUpperCase();
+// });
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+
+  
 });
+
+
+
+app.get('/project', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Projects',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+
 
 app.get('/about', (req, res) => {
-    res.render('about.hbs',{
-        pageTitle:'ABCD', 
-        currentYear: new Date().getFullYear(),
-        
-    });
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
 
-app.listen(port, () => {console.log(`Server Is Up On PORT ${port}`)});
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
